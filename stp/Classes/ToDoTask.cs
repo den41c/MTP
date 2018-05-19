37d72a9a5a025b00975cb06b0bf955b816c98fd5
Денис Коваленко <denskval@gmail.com>
2018-05-19 23:10:45 +0300

    -

--- stp/Classes/ToDoTask.cs
+++ stp/Classes/ToDoTask.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using System.Data.SQLite;
 namespace stp.Classes
 {
-    class Task
+    public class ToDoTask
     {
         public string TaskName { get; set; }
         
